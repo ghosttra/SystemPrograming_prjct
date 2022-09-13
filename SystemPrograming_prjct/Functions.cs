c0c6a34dd3b0c6f64fd48231ef8d7ad294647442
tr1ckshxt <skoromny.m@gmail.com>
2022-09-13 21:09:47 +0300

    Добавьте файлы проекта.

---
+++ SystemPrograming_prjct/Functions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace SystemPrograming_prjct
+{
+    public static class Functions
+    {
+        public const long YES_NO = 0x00000004L;
+
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        public static extern int MessageBox(IntPtr hWnd, String text, String caption, uint type);
+
+        public const int SW_HIDE = 0;
+        public const int SW_SHOWNORMAL = 1;
+        public const int SW_SHOWMINIMIZED = 2;
+        public const int SW_SHOWMAXIMIZED = 3;
+        public const int SW_SHOW = 5;
+
+        [DllImport("user32.dll")]
+        public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        [DllImport("user32.dll", EntryPoint = "FindWindow", SetLastError = true)]
+        public static extern IntPtr FindWindowByCaption(IntPtr ZeroOnly, string lpWindowName);
+
+
+        public const int WM_CLOSE = 16;
+        public const int WM_SETTEXT = 12;
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, UIntPtr wParam, StringBuilder lParam);
+
+        private static Random rng = new Random();
+        public static void Shuffle<T>(this IList<T> list)
+        {
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
+
+    }
+}
---
+++ SystemPrograming_prjct/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemPrograming_prjct
+{
+    internal class Program
+    {
+        enum YesNo
+        {
+            YES = 6,
+            NO = 7,
+        }
+        static void Main(string[] args)
+        {
+            //Ex1------------------------------------------------------------------------------------
+            int res = Functions.MessageBox(IntPtr.Zero, "Hello World", "Ex1", 0);
+            //Ex2------------------------------------------------------------------------------------
+            List<int> randomList = new List<int>();
+
+            for (int i = 0; i < 100; i++)
+            {
+                randomList.Add(i);
+            }
+
+            randomList.Shuffle();
+
+            for (int i = 0; i < randomList.Count; i++)
+            {
+                if (Functions.MessageBox(IntPtr.Zero, "Your digit is: " + randomList[i] + "?", "Digits guesser", (uint)Functions.YES_NO) == 6)
+                {
+                    Functions.MessageBox(IntPtr.Zero, "Congrats!", "Digits guesser", 0);
+                    break;
+                }
+            }
+            //Ex3------------------------------------------------------------------------------------
+
+            Console.Write("Enter window name: ");
+
+            var title = Console.ReadLine();
+            IntPtr handle = Functions.FindWindowByCaption(IntPtr.Zero, title);
+            Functions.SendMessage(handle, Functions.WM_CLOSE, UIntPtr.Zero, new StringBuilder());
+
+            //Ex4------------------------------------------------------------------------------------
+
+            Console.Write("Enter window name: ");
+
+            var title1 = Console.ReadLine();
+            IntPtr handle1 = Functions.FindWindowByCaption(IntPtr.Zero, title1);
+            Functions.SendMessage(handle1, Functions.WM_SETTEXT, UIntPtr.Zero, new StringBuilder(DateTime.Now.TimeOfDay.ToString()));
+
+            Console.ReadKey();
+        }
+}
+}
